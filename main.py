4290c738b1e6ac2fa35fb1f2c13c746886fb740d
Hezheng Yin <hezheng@meri.co>
2019-12-30 07:57:01 -0800

    Second commit (adding test1)

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 
 
+def test1():
+    # test1 is above initial Dev Eq threshold,
+    # has no modification after first commit,
+    # and has in-degree of 1
+    # but it lives shorter than 365 days
+    if os.name != "nt":
+        shutil.rmtree(top)
+        return
+
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            filename = os.path.join(root, name)
+            os.chmod(filename, stat.S_IWUSR)
+            os.remove(filename)
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(top)
+
+
 def test2():
     # test 2 lives longer than 365 days,
     # has no modification after first commit,
@@ -43,5 +62,6 @@ def test4():
     return sha
 
 
+test1()
 test2()
 test3()
